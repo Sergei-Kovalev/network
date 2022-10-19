073bc7377851bff423b7fb27cfd852082a1c3560
Sergei Kovalev <summerjob@ngs.ru>
2022-10-19 17:34:44 +0300

    Server handlers added.

--- src/main/java/ru/ngs/summerjob/net/Client.java
+++ src/main/java/ru/ngs/summerjob/net/Client.java
@@ -1,13 +1,12 @@
-package ru.ngs.summerjob;
+package ru.ngs.summerjob.net;
 
 import java.io.*;
 import java.net.Socket;
-import java.time.LocalDateTime;
 
 public class Client {
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; i++) {
-            SimpleClient sc = new SimpleClient();
+        for (int i = 0; i < 8; i++) {
+            SimpleClient sc = new SimpleClient(i);
             sc.start();
         }
     }
@@ -15,16 +14,24 @@ public class Client {
 
 class SimpleClient extends Thread {
 
+    private final static String[] COMMAND = {"HELLO", "MORNING", "DAY", "EVENING"};
+
+    private final int cmdNumber;
+    public SimpleClient(int cmdNumber) {
+        this.cmdNumber = cmdNumber;
+    }
+
     @Override
     public void run() {
         try {
-            System.out.println("Client started:" + LocalDateTime.now());
+//            System.out.println("Client started:" + LocalDateTime.now());
             Socket socket = new Socket("127.0.0.1", 25225);
 
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            String sb = "Sergei";
+            String command = COMMAND[cmdNumber % COMMAND.length];
+            String sb = command + " " + "Sergei";
 
             bw.write(sb);
             bw.newLine();
@@ -35,7 +42,7 @@ class SimpleClient extends Thread {
 
             bw.close();
             br.close();
-            System.out.println("Client finished:" + LocalDateTime.now());
+//            System.out.println("Client finished:" + LocalDateTime.now());
         } catch (IOException e) {
             e.printStackTrace(System.out);
         }
